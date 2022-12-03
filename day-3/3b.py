222e08739faf7fae3d907599e079adcd51936633
Pavel Kudlanov <pkudlanov@yahoo.com>
2022-12-03 06:39:04 -0500

    Day 3B Puzzle

--- day-3/3b.py
+++ day-3/3b.py
@@ -2,6 +2,7 @@ class PackOrganizer():
 
     def __init__(self, *args, **kwargs):
         self.sum_priorities = 0
+        self.threesome = []
 
     def __str__(self):
         return 'The sum of all the priorities is ' + str(self.sum_priorities) + '.'
@@ -11,13 +12,16 @@ class PackOrganizer():
         self.sum_priorities += item_alphabet.find(kwargs['common_item'])
 
     def find_common_item(self, *args, **kwargs):
-        num_items = int(len(kwargs['items']) / 2)
+        bags = kwargs['bags']
 
-        compartment_one = kwargs['items'][:num_items]
-        compartment_two = kwargs['items'][num_items:]
-
-        common_item = ''.join(list(set(compartment_one) & set(compartment_two)))
+        common_item = ''.join(list(set(bags[0]) & set(bags[1]) & set(bags[2])))
         self.add_priority(common_item=common_item)
+
+    def get_threesome(self, *args, **kwargs):
+        self.threesome.append(kwargs['items'])
+        if len(self.threesome) == 3:
+            self.find_common_item(bags=self.threesome)
+            self.threesome.clear()
 
 
 pack = PackOrganizer()
@@ -27,6 +31,6 @@ with open('input.txt') as f:
 
     for line in lines:
         items = line.splitlines()[0]
-        pack.find_common_item(items=items)
+        pack.get_threesome(items=items)
 
 print(pack)
